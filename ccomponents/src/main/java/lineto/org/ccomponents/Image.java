97a19874ec098a846a8af2f3f5160227f440257f
Anderson Marques <andersonmm@gmail.com>
2014-12-27 19:44:48 -0200

    Project creation, first commit.

---
+++ ccomponents/src/main/java/lineto/org/ccomponents/Image.java
@@ -0,0 +1,13 @@
+package lineto.org.ccomponents;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by amarques on 12/27/14.
+ */
+public class Image extends RelativeLayout {
+    public Image(Context context) {
+        super(context);
+    }
+}
